b0c13dcbe0a5c1ad5a9904fc3ca49080c3ffad4c
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-07-25 15:45:25 +0530

    added /neko

--- main.go
+++ main.go
@@ -1,75 +1,98 @@
 package main
 
 import (
-   "log"
+	"log"
 
-   "encoding/json"
-   "fmt"
-   "github.com/go-telegram-bot-api/telegram-bot-api"
-   "io/ioutil"
-   "net/http"
+	"encoding/json"
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"io/ioutil"
+	"net/http"
 )
 
 type Meme struct {
-   Image  string
-   Reddit string
-   Title  string
+	Image  string
+	Reddit string
+	Title  string
 }
 
+type Neko struct {
+   Url string
+}
+
+
 func main() {
-   token := ""
-   bot, err := tgbotapi.NewBotAPI(token)
-   if err != nil {
-      log.Panic(err)
-   }
+	token := ""
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
 
-   bot.Debug = true
+	bot.Debug = false
 
-   log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-   u := tgbotapi.NewUpdate(0)
-   u.Timeout = 60
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-   updates, err := bot.GetUpdatesChan(u)
+	updates, err := bot.GetUpdatesChan(u)
 
-   for update := range updates {
-      if update.Message == nil {
-         continue
-      }
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-      log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-      if update.Message.IsCommand() {
-         msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-         switch update.Message.Command() {
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
 
-         case "meme":
-            resp, err := http.Get("https://nksamamemeapi.pythonanywhere.com")
-            if err != nil {
-               fmt.Println(err)
-            } else {
-               data, _ := ioutil.ReadAll(resp.Body)
-               fmt.Println(string(data))
-               var meme Meme
-               json.Unmarshal([]byte(data), &meme)
-               fmt.Printf(meme.Title)
-               file := meme.Image
-               msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
-               msg.FileID = file
-               msg.Caption = meme.Title
-               msg.UseExisting = true
-               bot.Send(msg)
-            }
-         case "start":
-            msg.Text = "Hello"
-         case "help":
-            msg.Text = "/meme"
+			case "meme":
+				resp, err := http.Get("https://nksamamemeapi.pythonanywhere.com")
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					data, _ := ioutil.ReadAll(resp.Body)
+					fmt.Println(string(data))
+					var meme Meme
+					json.Unmarshal([]byte(data), &meme)
+					fmt.Printf(meme.Title)
+					file := meme.Image
+					msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
+					msg.FileID = file
+					msg.Caption = meme.Title
+					msg.UseExisting = true
+					bot.Send(msg)
+				}
+			case "start":
+				msg.Text = "Hello"
+			case "help":
+				msg.Text = "/meme"
 
-         default:
-            msg.Text = "I don't know that command"
-         }
-         bot.Send(msg)
-      }
+			case "neko":
+				res, err := http.Get("https://api.waifu.pics/sfw/neko")
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					data, _ := ioutil.ReadAll(res.Body)
+					// fmt.Println(string(data))
+					var cat Neko
+					json.Unmarshal([]byte(data), &cat)
+					fmt.Println(cat.Url)
+					nekochan := cat.Url
+					msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, nil)
+					msg.FileID = nekochan
+					msg.UseExisting = true
+					bot.Send(msg)
 
-   }
+				}
+
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
+
+	}
 }
